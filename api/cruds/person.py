c59f9e2494f807f86284168c0596dec6eaadb4b7
おがめ <kazuhiroisobe0522@gmail.com>
2023-03-13 19:00:32 +0900

    DBへのアクセスを完成させた

--- api/cruds/person.py
+++ api/cruds/person.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import api.models.person as person_model
 import api.schemas.person as person_schema
@@ -50,3 +50,34 @@ async def get_persons(db: AsyncSession) -> List[Tuple[int, str]]:
     )
 
     return result.all()
+
+async def get_person(db: AsyncSession, person_id: int) -> Optional[person_model.Person]:
+    result: Result = await db.execute(
+        select(person_model.Person).filter(person_model.Person.id == person_id)
+    )
+    person: Optional[Tuple[person_model.Person]] = result.first()
+    return person[0] if person is not None else None
+
+async def update_person(
+        db: AsyncSession, person_create: person_schema.PersonCreate, original: person_model.Person
+) -> person_model.Person:
+    original.first_name = person_create.name.first
+    original.last_name = person_create.name.last
+    original.age = person_create.age
+    original.sex = person_create.sex
+    original.region_prefecture = person_create.region.prefecture
+    original.region_city = person_create.region.city
+    original.favorite_lang_00 = person_create.favorite_language[0]
+    original.favorite_lang_01 = person_create.favorite_language[1]
+    original.favorite_lang_02 = person_create.favorite_language[2]
+    original.favorite_lang_03 = person_create.favorite_language[3]
+    original.favorite_lang_04 = person_create.favorite_language[4]
+
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
+
+async def delete_person(db: AsyncSession, original: person_model.Person) -> None:
+    await db.delete(original)
+    await db.commit()
--- api/routers/person.py
+++ api/routers/person.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.person as person_crud
@@ -53,10 +53,34 @@ async def create_person(
 
 
 @router.put('/persons/{person_id}', response_model=person_schema.PersonCreateResponse)
-async def update_person(person_id: int, person_body: person_schema.PersonCreate):
-    return person_schema.PersonCreateResponse(person_id, **person_body.dict())
+async def update_person(
+    person_id: int, person_body: person_schema.PersonCreate, db: AsyncSession = Depends(get_db)
+):
+    person = await person_crud.get_person(db, person_id=person_id)
+    if person is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    updated_person = await person_crud.update_person(db, person_body, original=person)
 
+    return person_schema.PersonCreateResponse(
+        id=updated_person.id,
+        name=person_schema.Name(first=updated_person.first_name, last=updated_person.last_name),
+        age=updated_person.age,
+        sex=updated_person.sex,
+        region=person_schema.Region(prefecture=updated_person.region_prefecture, city=updated_person.region_city),
+        favorite_language=[
+            updated_person.favorite_lang_00,
+            updated_person.favorite_lang_01,
+            updated_person.favorite_lang_02,
+            updated_person.favorite_lang_03,
+            updated_person.favorite_lang_04
+        ]
+    )
 
 @router.delete("/persons/{person_id}", response_model=None)
-async def delete_person(person_id: int):
-    return
+async def delete_person(person_id: int, db: AsyncSession = Depends(get_db)):
+    person = await person_crud.get_person(db, person_id=person_id)
+    if person is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return await person_crud.delete_person(db, original=person)
