6b6a0fdea7555d589b7adf5ea4a0a116146fe224
おがめ <kazuhiroisobe0522@gmail.com>
2023-03-14 19:15:34 +0900

    new

--- api/routers/certification.py
+++ api/routers/certification.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+# from requests_oauthlib import OAuth2Session
+from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
+CLIENT_ID = 'b811937cf10d9b379129'
+CLIENT_SECRET = '3b461ccaf29125635461541f506e175a20f15d32'
 
-@router.get('/login/github')
-async def login_github():
-    pass
+# github = OAuth2Session(CLIENT_ID, redirect_uri='http://localhost:8000/callback/github')
 
-@router.get('/login/github/callback')
-async def login_github_callback():
-    pass
+@router.get('/items/')
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {'token': token}
+
+# @router.get('/login/github')
+# async def login_github():
+#     pass
+
+# @router.get('/login/github/callback')
+# async def login_github_callback():
+#     pass
