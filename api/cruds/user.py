27950df4d65132e6dd1584aa776f302385b36041
おがめ <kazuhiroisobe0522@gmail.com>
2023-03-14 14:25:14 +0900

    言語複数選択での検索に対応

--- api/cruds/user.py
+++ api/cruds/user.py
@@ -89,7 +89,7 @@ async def delete_user(db: AsyncSession, original: user_model.User) -> None:
     await db.delete(original)
     await db.commit()
 
-def judge_equal(value, user_data) -> bool:
+def __judge_equal(value, user_data) -> bool:
     if value == None:
         return False
     elif value == user_data:
@@ -97,7 +97,7 @@ def judge_equal(value, user_data) -> bool:
     else:
         return False
     
-def judge_part_equal(value, user_data) -> bool:
+def __judge_part_equal(value, user_data) -> bool:
     if value == None:
         return False
     elif value in user_data:
@@ -107,48 +107,73 @@ def judge_part_equal(value, user_data) -> bool:
 
 async def search_user(
     db: AsyncSession, name: Optional[str], age: Optional[int], sex: Optional[int], prefecture: Optional[str], city: Optional[str], 
-    githubid: Optional[str], favolang: Optional[str], wantlang: Optional[str]
+    githubid: Optional[str], favolangs: Optional[List[str]], wantlangs: Optional[List[str]]
 ) -> List[user_schema.User]:
     users_list = await list_users(db)
     if name != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_part_equal(name, user.name.last + user.name.first):
+            if not __judge_part_equal(name, user.name.last + user.name.first):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
     if age != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_equal(age, user.age):
+            if not __judge_equal(age, user.age):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
     if sex != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_equal(sex, user.sex):
+            if not __judge_equal(sex, user.sex):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
     if prefecture != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_equal(prefecture, user.region.prefecture):
+            if not __judge_equal(prefecture, user.region.prefecture):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
     if city != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_equal(city, user.region.city):
+            if not __judge_equal(city, user.region.city):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
     if githubid != None:
+        if len(users_list) == 0:
+            return []
         for i, user in enumerate(users_list):
-            if not judge_part_equal(githubid, user.github_id):
+            if not __judge_part_equal(githubid, user.github_id):
                 users_list[i] = None
         users_list = [u for u in users_list if u is not None]
-    if favolang != None:
-        for i, user in enumerate(users_list):
-            if not judge_part_equal(favolang, user.language.favorite):
-                users_list[i] = None
+    if favolangs != None:
+        for lang in favolangs:
+            if lang != None:
+                if len(users_list) == 0:
+                    return []
+                for i, user in enumerate(users_list):
+                    if user == None:
+                        continue
+                    if not __judge_part_equal(lang, user.language.favorite):
+                        users_list[i] = None
+                    print(i)
         users_list = [u for u in users_list if u is not None]
-    if wantlang != None:
-        for i, user in enumerate(users_list):
-            if not judge_part_equal(wantlang, user.language.want_to):
-                users_list[i] = None
+    if wantlangs != None:
+        for lang in wantlangs:
+            if lang != None:
+                if len(users_list) == 0:
+                    return []
+                for i, user in enumerate(users_list):
+                    if user == None:
+                        continue
+                    if not __judge_part_equal(lang, user.language.want_to):
+                        users_list[i] = None
         users_list = [u for u in users_list if u is not None]
 
     return users_list
--- api/routers/user.py
+++ api/routers/user.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.user as user_crud
+import api.cruds.matching as matching_crud
 from api.db import get_db
 
 import api.schemas.user as user_schema
@@ -89,15 +90,18 @@ async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
 
 @router.get('/search/', response_model=List[user_schema.User])
 async def search_user(
-    name: Optional[str] = None, age: Optional[int] = None, sex: Optional[int] = None, prefecture: Optional[str] = None, city: Optional[str] = None, 
-    githubid: Optional[str] = None, favolang: Optional[str] = None, wantlang: Optional[str] = None, db: AsyncSession = Depends(get_db)
+    name: Optional[str] = None, age: Optional[int] = None, sex: Optional[int] = None, prefecture: Optional[str] = None, city: Optional[str] = None, githubid: Optional[str] = None, 
+    favolang0: Optional[str] = None, favolang1: Optional[str] = None, favolang2: Optional[str] = None, favolang3: Optional[str] = None, favolang4: Optional[str] = None, 
+    wantlang0: Optional[str] = None, wantlang1: Optional[str] = None, wantlang2: Optional[str] = None, wantlang3: Optional[str] = None, wantlang4: Optional[str] = None, 
+    db: AsyncSession = Depends(get_db)
 ) -> List[user_schema.User]:
     return await user_crud.search_user(
         db=db, name=name, age=age, sex=sex, prefecture=prefecture, city=city, 
-        githubid=githubid, favolang=favolang, wantlang=wantlang
+        githubid=githubid, 
+        favolangs=[favolang0, favolang1, favolang2, favolang3, favolang4], 
+        wantlangs=[wantlang0, wantlang1, wantlang2, wantlang3, wantlang4]
     )
 
 @router.get('/matching/{user_id}', response_model=List[user_schema.User])
-async def matching_users(user_id: int, db: AsyncSession = Depends(get_db)):
-    pass
-
+async def matching_users(user_id: int, num: Optional[int] = None, db: AsyncSession = Depends(get_db)):
+    return await matching_crud.matching_users(db, user_id, num)
